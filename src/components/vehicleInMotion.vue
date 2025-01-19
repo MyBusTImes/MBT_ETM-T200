e5c1854161e10a6bba3b792eb64daa59b44cb3e5
Nova-Spectra-Games <nsg-group@nova-spectra-games.com>
2025-01-19 00:42:35 +0000

    Update vehicleInMotion.vue
    Made the Arrive at button work

--- src/components/vehicleInMotion.vue
+++ src/components/vehicleInMotion.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+import { updateCurrentStop } from '@/update_currect_stop.js';
+
 export default {
     data() {
         return {
@@ -20,17 +22,20 @@ export default {
         }
     },
     mounted() {
+        const TripID = localStorage.getItem('TripID');
+        this.TripID = TripID
         // Ensure currentIndexStop is a valid number
         const currentIndexStop = Number(localStorage.getItem('currentIndexStop')) || 0;
         console.log(currentIndexStop);
+        this.currentIndexStop = currentIndexStop;
 
         // Get the route stops from localStorage and parse it as a JSON array
         const stopArray = JSON.parse(localStorage.getItem('stopArray')) || [];
         this.stopArray = stopArray;
 
         // Ensure the stopArray has valid stops and calculate the next stop index
-        if (this.stopArray.length > 0 && currentIndexStop + 1 < this.stopArray.length) {
-            this.nextStop = this.stopArray[currentIndexStop + 1]; // Get next stop
+        if (this.stopArray.length > 0 && this.currentIndexStop + 1 < this.stopArray.length) {
+            this.nextStop = this.stopArray[this.currentIndexStop]; // Get next stop
         } else {
             this.nextStop = 'No more stops'; // Fallback if no more stops are available
         }
@@ -41,6 +46,10 @@ export default {
             const audio = new Audio(require('@/assets/Audio/Click.wav'));
             audio.play();
             console.log("Arriving at stop:", this.nextStop);
+            updateCurrentStop(this.TripID, this.stopArray[this.currentIndexStop]);
+            this.currentIndexStop = this.currentIndexStop + 1;
+            localStorage.setItem('currentIndexStop', this.currentIndexStop)
+            this.nextStop = this.stopArray[this.currentIndexStop];
         },
         goToTicketSelling() {
             this.$router.go(-1);
